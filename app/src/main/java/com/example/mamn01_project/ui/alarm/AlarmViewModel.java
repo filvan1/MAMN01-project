73a2aa4563265c29fcee51b16e2627d9ba8d88de
Filip Vannfält <filip.vannfalt@gmail.com>
2023-04-25 14:51:03 +0200

    Base navigation structure with fragments

---
+++ app/src/main/java/com/example/mamn01_project/ui/alarm/AlarmViewModel.java
@@ -0,0 +1,19 @@
+package com.example.mamn01_project.ui.alarm;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AlarmViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public AlarmViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is alarm fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
