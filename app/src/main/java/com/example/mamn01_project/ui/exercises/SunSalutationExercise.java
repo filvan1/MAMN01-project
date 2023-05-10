ad9e5fa0ffaa36b688e4be75577f2dc2d855fb9a
Gustav94 <gustav_t94@hotmail.com>
2023-05-10 16:35:38 +0200

    Adderat solhälsning till poolen

--- app/src/main/java/com/example/mamn01_project/ui/exercises/SunSalutationExercise.java
+++ app/src/main/java/com/example/mamn01_project/ui/exercises/SunSalutationExercise.java
@@ -3,15 +3,14 @@ package com.example.mamn01_project.ui.exercises;
 import android.annotation.SuppressLint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
-import android.os.Parcel;
 
 import androidx.annotation.NonNull;
 
 
-public class SunSalutationExercise extends Exercise{
-    private float highTarget;
-    private float lowTarget;
-
+public class SunSalutationExercise extends Exercise {
+    private boolean neutralPosition = false;
+    private boolean touchedToes = false;
+    private boolean phoneOverHead = false;
 
     public SunSalutationExercise(String name) {
         super(name);
@@ -19,16 +18,27 @@ public class SunSalutationExercise extends Exercise{
 
     @Override
     public int requiredSensorType() {
-       return  Sensor.TYPE_GYROSCOPE;
+        return Sensor.TYPE_ACCELEROMETER;
     }
 
     @Override
     public boolean isCompleted() {
-        return false;
+        return neutralPosition && touchedToes && phoneOverHead;
     }
 
     @Override
     public void processSensorEvent(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float zValue = sensorEvent.values[2];
 
+            if (!neutralPosition && zValue > -2 && zValue < 2) {
+                neutralPosition = true;
+            } else if (neutralPosition && !touchedToes && zValue > 9) {
+                touchedToes = true;
+            } else if (neutralPosition && touchedToes && zValue < -9) {
+                phoneOverHead = true;
+            }
+        }
     }
 }
+
