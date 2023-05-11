69f64c9b16a7c983e2d0c296f7a4fbfb4f8621be
Alexander Bartosik <alex.bartosik@gmail.com>
2023-05-11 13:01:39 +0200

    SunSalutationExercise implementation med gyro

--- app/src/main/java/com/example/mamn01_project/ui/exercises/SunSalutationExercise.java
+++ app/src/main/java/com/example/mamn01_project/ui/exercises/SunSalutationExercise.java
@@ -3,6 +3,7 @@ package com.example.mamn01_project.ui.exercises;
 import android.annotation.SuppressLint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -12,6 +13,14 @@ public class SunSalutationExercise extends Exercise {
     private boolean neutralPosition = false;
     private boolean touchedToes = false;
     private boolean phoneOverHead = false;
+
+    private float[] magnetic = null;
+    private float[] accelerometer = null;
+    private double reps;
+    private final double FINAL_REPS = 5;
+    private boolean completed = false;
+
+
 
     public SunSalutationExercise(String name) {
         super(name);
@@ -24,12 +33,13 @@ public class SunSalutationExercise extends Exercise {
 
     @Override
     public boolean isCompleted() {
-        return neutralPosition && touchedToes && phoneOverHead;
+        return completed;
+        //return neutralPosition && touchedToes && phoneOverHead;
     }
 
     @Override
     public void processSensorEvent(SensorEvent sensorEvent) {
-        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+        /*if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             float zValue = sensorEvent.values[2];
 
             if (!neutralPosition && zValue > -2 && zValue < 2) {
@@ -39,7 +49,33 @@ public class SunSalutationExercise extends Exercise {
             } else if (neutralPosition && touchedToes && zValue < -9) {
                 phoneOverHead = true;
             }
+        }*/
+
+        float[] rotationMatrix = new float[9];
+        float[] inclinationMatrix = new float[9];
+
+        if ((magnetic != null) && (sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION)) {
+
+            SensorManager.getRotationMatrix(rotationMatrix, inclinationMatrix, accelerometer, magnetic);
+            int inclination = (int) Math.round(Math.toDegrees(Math.acos(rotationMatrix[8])));
+            if (reps == FINAL_REPS) {
+                setCompleted(true);
+            } else if (inclination < 90) {
+                //face up
+                reps += 0.5;
+            } else if (inclination > 90) {
+                //face down
+                reps += 0.5;
+            }
+        //om magnetometern uppdateras, spara värdet
+        } else if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            magnetic = sensorEvent.values;
         }
+        Log.d("SunSalutationExercise", "processorSensorEvent: reps" + reps);
+    }
+
+    private void setCompleted(boolean setter) {
+        completed = setter;
     }
 }
 
